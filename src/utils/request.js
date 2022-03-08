80f53ebc5bda9d83a55aa99cfadedfb06c748ea3
ZHOUYUANN <464814413@qq.com>
2022-03-08 09:46:51 +0800

    新增 axios 接口封装

---
+++ src/utils/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+
+const http = axios.create({
+  baseUrl: process.env.VUE_APP_BASE_API,
+  timeout: 5000
+})
+
+// 设置相应拦截器
+http.interceptors.response.use(
+  (res) => {
+    const { success, message, data } = res.data
+    // 可以用 success 或者 code 来判断用户是否是失败的状态，根据项目来
+    if (success) {
+      return data
+    } else {
+      ElMessage.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  (err) => {
+    ElMessage.error(err)
+    return Promise.reject(err)
+  }
+)
