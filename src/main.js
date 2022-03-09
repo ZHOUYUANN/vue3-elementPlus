9480336e76f668d9c3c99aa447d41c25b7f3e37c
ZHOUYUANN <464814413@qq.com>
2022-03-09 17:22:02 +0800

    新增：加入 element 全局样式引入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-plus/theme-chalk/index.css'
 
 import './permission'
 // 全局样式文件
